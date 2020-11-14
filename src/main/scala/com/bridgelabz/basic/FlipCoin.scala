e59bd34cea6267d95ab03e67c4b92922100995fb
Rajat Gundi <glrajat@gmail.com>
2020-11-14 17:02:20 +0530

    Refractored: Added comments to Flip Coin Program

--- src/main/scala/com/bridgelabz/basic/FlipCoin.scala
+++ src/main/scala/com/bridgelabz/basic/FlipCoin.scala
@@ -1,23 +1,29 @@
 package com.bridgelabz.basic
 
 object FlipCoin extends App {
+  //Initializing no. of tosses done to 0
   var coinTossCount: Int = 0
+
+  //Input number of tosses to be done from user
   println("Enter the Coinflip Count: ")
   val flipCount = scala.io.StdIn.readInt()
 
-  def coinflip(): Int = {
+  //Function to toss the coin: 0 for tail, 1 for head
+  def coinFlip(): Int = {
     val r = scala.util.Random
-    var rand = r.nextInt(2)
     coinTossCount += 1
-    return rand
+    r.nextInt(2)
   }
 
+  //Use the loop to toss the coin flipCount no. of times
   var i = 0
   var numberOfHeads: Int = 0
   for (i <- 1 to flipCount) {
-    numberOfHeads += coinflip()
+    numberOfHeads += coinFlip()
   }
   val numberOfTails: Int = flipCount - numberOfHeads
+
+  //Display respective percentages
   println("Percentage for Heads: " + numberOfHeads * 100 / flipCount + "%")
   println("Percentage for Tails: " + numberOfTails * 100 / flipCount + "%")
 }

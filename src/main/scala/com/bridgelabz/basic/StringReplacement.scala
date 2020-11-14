32186600cb0441ebaf958a8d3b4611a994e29cbb
Rajat Gundi <glrajat@gmail.com>
2020-11-14 14:12:02 +0530

    Added: String Replacement

---
+++ src/main/scala/com/bridgelabz/basic/StringReplacement.scala
@@ -0,0 +1,16 @@
+package com.bridgelabz.basic
+object StringReplacement extends App {
+  //String Template
+  val userMessage:String = "Hello <<UserName>>, how are you?"
+  //Read User Input
+  println("What's your Name?")
+  val userName:String = scala.io.StdIn.readLine()
+  //Input atleast 3 chars
+  if(userName.length() <= 3){
+    println("Invalid username.")
+  }
+  else{
+    val display = userMessage.replaceAll("<<[\\s\\S]*>>", userName)
+    println(display)
+  }
+}

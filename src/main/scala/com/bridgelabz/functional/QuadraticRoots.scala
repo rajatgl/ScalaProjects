05ad62f7cd05cc519322050a40845fdeef9d8543
Rajat Gundi <glrajat@gmail.com>
2020-11-17 10:36:52 +0530

    Added: Quadratic Roots Program

---
+++ src/main/scala/com/bridgelabz/functional/QuadraticRoots.scala
@@ -0,0 +1,33 @@
+package com.bridgelabz.functional
+
+object QuadraticRoots {
+
+  def quadraticRoots(a:Int, b:Int, c:Int):Array[Double] = {
+    val delta:Double = Math.pow(b,2) - (4 * a * c)
+    val roots:Array[Double] = Array.ofDim[Double](2)
+
+    if(delta < 0)
+      println("Returning NaN as delta < 0");
+
+    roots(0) = (-b + Math.sqrt(delta)/(2*a))
+    roots(1) = (-b - Math.sqrt(delta)/(2*a))
+
+    roots
+  }
+
+  def coefficients():Array[Int] = {
+    val coeffs = Array.ofDim[Int](3)
+    print("Enter A: ")
+    coeffs(0) = scala.io.StdIn.readInt()
+    print("Enter B: ")
+    coeffs(1) = scala.io.StdIn.readInt()
+    print("Enter C: ")
+    coeffs(1) = scala.io.StdIn.readInt()
+    coeffs
+  }
+
+  def main(args: Array[String]): Unit = {
+    val coeffs = coefficients()
+    println(quadraticRoots(coeffs(0), coeffs(1), coeffs(2)).mkString("Roots: (", ", ", ")"))
+  }
+}
